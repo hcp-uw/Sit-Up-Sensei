18f9747c4352e58a361ed7eb515cf945fba695e1
simku22 <simku22@uw.edu>
2024-03-04 17:02:04 -0800

    Added detailed navbar (still needs to collapse/grow) and better class structure

--- situp-sensei/src/App.js
+++ situp-sensei/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from './LandingPage.js';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">
-          Sit-Up Sensei
-        </h1>
-        <p className="PrimaryButton">
-          CREATE ACCOUNT
-        </p>
-        <p className="SecondaryButton">
-          LOG IN
-        </p>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
   );
 }
 
--- situp-sensei/src/index.js
+++ situp-sensei/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import NavBar from './NavBar';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <NavBar />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
