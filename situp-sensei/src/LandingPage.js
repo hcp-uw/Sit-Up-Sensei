edfb6ea939b7ecb43ed58e2f0151c0cc04d664d8
simku22 <simku22@uw.edu>
2024-03-06 02:41:33 -0800

    added fancy animations to our landing page

--- situp-sensei/src/LandingPage.js
+++ situp-sensei/src/LandingPage.js
@@ -1,27 +1,71 @@
 import logo from './logo.svg';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 import { motion } from 'framer-motion';
+import { TypeAnimation } from 'react-type-animation';
+
+const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`;
+const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`;
+
+function Image(props) {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [isInView, setIsInView] = useState(false);
+
+    return (
+        <section>
+          <motion.div
+            initial={false}
+            animate={
+              isLoaded && isInView
+                ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+                : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+            }
+            transition={{ duration: 0.5, delay: 0.5 }}
+            viewport={{ once: true }}
+            onViewportEnter={() => setIsInView(true)}
+          >
+            <img src={props.id} className={props.classText} alt="" onLoad={() => setIsLoaded(true)} />
+          </motion.div>
+        </section>
+    );
+}
+
+function TypingText() {
+    return (
+        <TypeAnimation
+            preRenderFirstString={false}
+            sequence={[
+                500,
+                'VERY STRONK',
+                1000,
+                'NECK GROW BIG',
+                1000,
+                'BADA BING BADA BOOM',
+                1000,
+                'HAIYA! POW!',
+                500,
+            ]}
+            speed={10}
+            style={{
+                fontSize: '1rem',
+                fontFamily:  "Saira",
+                paddingBottom: '1rem',
+            }}
+            repeat={Infinity}
+        />
+    );
+}
 
 function LandingPage() {
     return (
         <div className="App">
             <header className="App-header">
-                <motion.img
-                    initial={{ pathLength: 0 }}
-                    animate={{ pathLength: 1 }}
-                    src={logo}
-                    className="App-logo"
-                    alt="logo"
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 1.1 }}
-                />
+                <Image id={logo} classText="App-logo"/>
                 <h1 className="App-title">
                     Sit-Up Sensei
                 </h1>
-                <p>
-
-                </p> 
+                <TypingText />
                 <Link to="/">
                     <p className="PrimaryButton">
                         CREATE ACCOUNT
